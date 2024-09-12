0e7eeaa93a52bf067d60edfa07752ecf8c94a794
David <82981068+davdsm@users.noreply.github.com>
2024-09-13 00:47:59 +0100

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,42 +1,27 @@
-let login = <boolean>false;
-let env = {
-  api: {
-    key: "d41d8cd98f00b204e9800998ecf8427e",
-    header: "davdsmKey",
-    address: "https://backend.davdsm.pt",
-    email: "https://mailer.davdsm.pt",
-  },
-  pocketBase: {
-    username: "joanigrave",
-    password: "PocketBase2k23_!!",
-  },
-};
+function sendEmail(parents) {
+  console.log("");
+  console.log("⚡ SMS Sent - ", parents.length);
+  console.log("⚡ Done for today - ", new Date());
 
-const myHeaders = new Headers();
-myHeaders.append(env.api.header, env.api.key);
-myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-
-export const sendEmail = async (
-  name: string,
-  email: string,
-  subject: string,
-  message: string
-): Promise<number> => {
-  const urlencoded = new URLSearchParams();
-  urlencoded.append("name", name);
-  urlencoded.append("email", email);
-  urlencoded.append("subject", subject);
-  urlencoded.append("message", message);
-
-  const requestOptions: RequestInit = {
+  // send email noticing admin
+  request({
+    url: "http://api.davdsm.pt/sendMail",
     method: "POST",
-    headers: myHeaders,
-    body: urlencoded,
-    redirect: "follow",
-  };
-
-  return fetch(`${env.api.email}/send`, requestOptions)
-    .then((response) => response.text())
-    .then(() => 200)
-    .catch(() => 403);
-};
+    headers: {
+      'Content-Type': 'application/json',
+      "davdsmKey": 'd41d8cd98f00b204e9800998ecf8427e'  // <--Very important!!!
+    },
+    body: JSON.stringify({
+      sender: "⭐ StarDancers",
+      receiver: {
+        email: debug ? "samuel_david_8@hotmail.com" : "stardancers2017@gmail.com",
+        name: "Admnistradora Ana"
+      },
+      subject: `✈️ (${parents.length}) SMS Enviados`,
+      message: `<h3>Olá Ana</h3><p>Este mês sairam ${parents.length} mensagens, segue a lista de pais que receberam o aviso de não pagamento:<br/> ${parents.map(parent => `${parent} <br/>`)}</p>Contacto>    })
+  }, function (error, response, body) {
+    console.log("✈️ Email Enviado? - ", response.body);
+    console.log("---------------------------------------------------");
+    console.log("");
+  });
+}
